670aa3af095ff66e12a304b65b43ca857ce2a2f4
0motola <oluwajoola@gmail.com>
2022-10-13 11:39:37 -0700

    low level programming tasks 0 to 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/**
+*main - Serves as the entry point for the program
+*
+*Return: By default returns 0 if errors otherwise returns non-zer value
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+my first Hello World with C
