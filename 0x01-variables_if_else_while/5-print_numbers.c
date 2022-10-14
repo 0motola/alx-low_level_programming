211e18c3c18384ef264bc36bb47f6685875cce3f
0motola <oluwajoola@gmail.com>
2022-10-14 16:59:07 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main- prints a random number and states if it is poositive, negative or zero.
+*
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
