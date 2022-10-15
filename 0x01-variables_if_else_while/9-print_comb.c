52d355615a6c14c922384f5b494f833774223473
0motola <oluwajoola@gmail.com>
2022-10-14 17:30:42 -0700

    all doooone

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+*main- prints a random number and states if it is poositive, negative or zero.
+*
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+		{
+		continue;
+		putchar(',');
+		putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
