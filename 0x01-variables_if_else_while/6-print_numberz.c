70264174d8c4220eaeb0552eef7ce423f5b7c6fe
0motola <oluwajoola@gmail.com>
2022-10-14 17:06:59 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main- prints a random number and states if it is poositive, negative or zero.
+*
+*Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
